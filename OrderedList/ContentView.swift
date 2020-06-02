13e2aa822ebbfc4323277fad37d741d643cfd647
elhajjaji <elhajjajiadiba@gmail.com>
2020-06-02 12:13:24 +0100

    Initial Commit

---
+++ OrderedList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OrderedList
+//
+//  Created by Elhajjaji on 6/2/20.
+//  Copyright © 2020 Elhajjaji. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
